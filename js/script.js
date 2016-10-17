bd22ed7056fe53c0a46415ff48fd3797314450a9
Jonathan Huxhold <jhuxhold@gmail.com>
2016-10-16 22:06:30 -0600

    adding email collection

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  var firstName = $('.first-name');
+  var lastName = $('.last-name');
+  var school = $('.school');
+  var district = $('.district');
+  var email = $('.email');
+  var state = $('.state');
+
   var myPlayer = videojs('vid1');
 
   $(".contentContainer").css("min-height", $(window).height());
@@ -14,6 +21,19 @@ $(document).ready(function() {
     return myPlayer.paused() ? myPlayer.play() : myPlayer.pause();
   });
 
+  $('.submit-email').click(function(){
+    var newEmailKey = fb.database().ref().child('emails').push().key;
+    var newEmail = {};
 
+    newEmail['/emails/' + newEmailKey] = {
+      first_name: firstName.val(),
+      last_name: lastName.val(),
+      school: school.val(),
+      district: district.val(),
+      email: email.val(),
+      state: state.val()
+    };
+    return fb.database().ref().update(newEmail);
+  })
 
 })
